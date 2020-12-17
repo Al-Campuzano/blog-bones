818608598b564df21f9c0cc2ed5819afe0fbb778
Alberto Campuzano <al.campuzano@shopify.com>
2020-12-17 15:38:31 -0500

    refactor blurb footer and child components to use hooks

--- src/components/BlurbFooter/BlurbFooter.js
+++ src/components/BlurbFooter/BlurbFooter.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CommentButton from '../CommentButton/CommentButton';
 import DateAndAuthor from '../DateAndAuthor/DateAndAuthor';
 
-class BlurbFooter extends Component {
-  render() {
-    const someDate = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
-    return (
-      <div>
-        <p>
-          <DateAndAuthor date={someDate.toLocaleDateString()} author="Riker" /> 
-          &nbsp;|&nbsp;  
-          <CommentButton />
-        </p>
-      </div>
-    );
-  }
+function BlurbFooter() {
+  const someDate = new Date(Date.UTC(2012,11,20,3,0,0));
+  return (
+    <div>
+      <p>
+        <DateAndAuthor date={someDate.toLocaleDateString()} author="Riker" /> 
+        <span> | </span>  
+        <CommentButton />
+      </p>
+    </div>
+  );
 }
 
 export default BlurbFooter;
--- src/components/DateAndAuthor/DateAndAuthor.js
+++ src/components/DateAndAuthor/DateAndAuthor.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class DateAndAuthor extends Component {
-  render() {
-    const date = this.props.date
-    const author = this.props.author
-    return (
-      <span>
-        {date}
-        &nbsp;by <a href="https://memory-alpha.fandom.com/wiki/William_T._Riker" target="_blank"
-          rel="noopener noreferrer">{author}</a>
-      </span>
-    );
-  }
+function DateAndAuthor(props) {
+  const date = props.date
+  const author = props.author
+  return (
+    <span>
+      {date}
+      &nbsp;by <a href="https://memory-alpha.fandom.com/wiki/William_T._Riker" target="_blank"
+        rel="noopener noreferrer">{author}</a>
+    </span>
+  );
 }
 
 export default DateAndAuthor;
