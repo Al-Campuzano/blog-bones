30749c5741f85e7fcfa9a35f386df9cf6c963d6d
Alberto Campuzano <al.campuzano@shopify.com>
2020-12-17 11:04:16 -0500

    add date and author to blurb footer

--- src/components/BlurbFooter/BlurbFooter.js
+++ src/components/BlurbFooter/BlurbFooter.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from 'react'
+import DateAndAuthor from '../DateAndAuthor/DateAndAuthor';
 
-const BlurbFooter = () => {
-  return <div />;
-};
+class BlurbFooter extends Component {
+  render() {
+    const someDate = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+    return (
+      <div>
+        <p><DateAndAuthor someDate={someDate.toLocaleDateString()} author="Riker" /> | <span>Comment button</span></p>
+      </div>
+    );
+  }
+}
 
 export default BlurbFooter;
--- src/components/DateAndAuthor/DateAndAuthor.js
+++ src/components/DateAndAuthor/DateAndAuthor.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-const DateAndAuthor = () => {
-  return <div />;
-};
+class DateAndAuthor extends Component {
+  render() {
+    const someDate = this.props.someDate
+    const author = this.props.author
+    return (
+      <span>
+        <span>{someDate}</span> <span>by {author}</span>
+      </span>
+    );
+  }
+}
 
 export default DateAndAuthor;
