ed3c4320ee910eb0c1209afec6313a121c931e2b
Alberto Campuzano <al.campuzano@shopify.com>
2021-01-06 16:16:28 -0500

    finish tests for NavContainer

--- src/components/NavContainer/tests/NavContainer.test.js
+++ src/components/NavContainer/tests/NavContainer.test.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import {mount} from '@shopify/react-testing'
 import NavContainer from '../'
+import {Link} from 'react-router-dom'
+import {toContainReactComponent} from '@shopify/react-testing/matchers';
 
 describe('<NavContainer />', () => {
-  it('testing tests', () => {
+  it('has 4 menu items', () => {
     const wrapper = mount(<NavContainer />);
-    expect(wrapper.text()).toContain('Blog');
+    expect(wrapper.find('nav').findAll('li').length).toBe(4);
   });
+
+  it('has 4 Link components', () => {
+    const wrapper = mount(<NavContainer />);
+    expect(wrapper).toContainReactComponentTimes(Link, 4);
+  });
+
+  it('has a link to homepage', () => {
+    const wrapper = mount(<NavContainer />);
+    expect(wrapper).toContainReactComponent(Link, {
+      to:'/',
+    });
+  });
+
+  it('has a link to the all posts page', () => {
+    const wrapper = mount(<NavContainer />);
+    expect(wrapper).toContainReactComponent(Link, {
+      to:'/posts',
+    });
+  });
+  
+  it('has a link to the new post page', () => {
+    const wrapper = mount(<NavContainer />);
+    expect(wrapper).toContainReactComponent(Link, {
+      to:'/new',
+    });
+  });
+  
+  it('has a link to the polaris page', () => {
+    const wrapper = mount(<NavContainer />);
+    expect(wrapper).toContainReactComponent(Link, {
+      to:'/polaris',
+    });
+  });
+
 });
