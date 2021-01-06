5840dd4473a7432b5b07650c374890307d713a1b
Alberto Campuzano <al.campuzano@shopify.com>
2021-01-06 17:25:01 -0500

    add tests for DateAndAuthor component

--- src/components/DateAndAuthor/tests/DateAndAuthor.test.js
+++ src/components/DateAndAuthor/tests/DateAndAuthor.test.js
@@ -1,3 +1,17 @@
 import React from 'react'
 import {mount} from '@shopify/react-testing'
 import '@shopify/react-testing/matchers';
+import DateAndAuthor from '../DateAndAuthor';
+
+describe('<DateAndAuthor />', () => {
+  it('date prop is displayed when present', () => {
+    const someDate = new Date(Date.UTC(2012,11,20,3,0,0));
+    const wrapper = mount(<DateAndAuthor date={someDate.toLocaleDateString()}/>);
+    expect(wrapper.find('span').text()).toContain('2012');
+  });
+
+  it('author prop is displayed when present', () => {
+    const wrapper = mount(<DateAndAuthor author="Testing" />);
+    expect(wrapper.find('a').text()).toBe('Testing');
+  });
+});
